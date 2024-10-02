1d53317faae9277dcca5b5b5193061f6a71305ed
Charansaiponnada <charansaiponnada06@gmail.com>
2024-10-02 17:20:12 +0530

    sorting.c

--- sorting.c
+++ sorting.c
@@ -1,18 +1,10 @@
 //week 6 and p-2
 #include <stdio.h>
 #include <stdlib.h>
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
 int partition(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = low - 1;
-    for (int j = low; j < high; j++) {
+    for ( j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
             int temp = arr[i];
@@ -25,13 +17,11 @@ int partition(int arr[], int low, int high) {
     arr[high] = temp;
     return i + 1;
 }
-
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
@@ -39,10 +29,10 @@ void merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
     int L[n1], R[n2];
-    for (int i = 0; i < n1; i++) {
+    for ( i = 0; i < n1; i++) {
         L[i] = arr[left + i];
     }
-    for (int j = 0; j < n2; j++) {
+    for (j = 0; j < n2; j++) {
         R[j] = arr[mid + 1 + j];
     }
     int i = 0, j = 0, k = left;
@@ -67,6 +57,15 @@ void merge(int arr[], int left, int mid, int right) {
         k++;
     }
 }
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
 
 int main() {
     int n, choice;
